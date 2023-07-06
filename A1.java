c22017909b8ab54d7676cbd02dbe189338e9bbe4
unknown <angadvaiday@gmail.com>
2023-07-06 16:09:34 +0530

    i am angad

---
+++ A1.java
@@ -0,0 +1,12 @@
+public class A1{
+int roll;
+String name;
+void R(int roll,String name){
+System.out.println("Roll no: "+ roll + " Name: "+name);
+}
+public static void main(String[] args){
+A1 H=new A1(); 
+H.R(45,"angad");
+}
+}
+
